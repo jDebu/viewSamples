669b1b7ec57abf7e7cb4e1bb4437baec07be0c24
jdebu <j2delgadob@gmail.com>
2017-05-18 11:24:23 -0500

    create login django auth

--- viewflowSample1/viewflowSample1/urls.py
+++ viewflowSample1/viewflowSample1/urls.py
@@ -1,22 +1,15 @@
-"""viewflowSample1 URL Configuration
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.11/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
-"""
 from django.conf.urls import url
 from django.contrib import admin
 from pqrs import  views
+from django.contrib.auth.views import login,logout
+from django.views import generic
+from django.contrib.auth.decorators import login_required
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^pqrs/', views.testView.as_view())
+    url('^$', generic.TemplateView.as_view(template_name='index.html')),
+    url(r'^pqrs/', login_required(views.testView.as_view())),
+    url(r'^login/', login, name='login'),
+    url(r'^logout/$', logout,name='logout'),
 ]
